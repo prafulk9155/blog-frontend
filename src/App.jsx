66e2e5b1b3746335dd9652925e9c3733b50cb629
praful kumar <prafulk9155@gmail.com>
2025-01-15 11:18:00 +0530

    vercel json updated for route

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Testing from './Components/Testing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    // <>
      
-      <h1>Blogging Platform with Markdown Support</h1>
+    //   <h1>Blogging Platform with Markdown Support</h1>
       
-    </>
+    // </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/test" element={<Testing />} />
+      <Route path="/" element={ <h1>Blogging Platform with Markdown Support</h1>} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
